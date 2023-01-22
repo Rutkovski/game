f11af83874f5492dc785152c8d69978d7eccf5b5
Rutkovski Dmitriy <rutkovski.dp@gmail.com>
2023-01-22 13:48:01 +0300

    init

---
+++ src/main/java/com/game/entity/Player.java
@@ -0,0 +1,121 @@
+package com.game.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "player")
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", length = 12)
+    private String name;
+
+    @Column(name = "title", length = 30)
+    private String title;
+
+    @Column(name = "race", length = 20)
+    private String race;
+
+    @Column(name = "profession", length = 20)
+    private String profession;
+
+    @Column(name = "birthday")
+    private LocalDate birthday;
+
+    @Column(name = "banned")
+    private Boolean banned;
+
+    @Column(name = "experience")
+    private Integer experience;
+
+    @Column(name = "level")
+    private Integer level;
+
+    @Column(name = "untilNextLevel")
+    private Integer untilNextLevel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Boolean getBanned() {
+        return banned;
+    }
+
+    public void setBanned(Boolean banned) {
+        this.banned = banned;
+    }
+
+    public Integer getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Integer experience) {
+        this.experience = experience;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getUntilNextLevel() {
+        return untilNextLevel;
+    }
+
+    public void setUntilNextLevel(Integer untilNextLevel) {
+        this.untilNextLevel = untilNextLevel;
+    }
+
+}
